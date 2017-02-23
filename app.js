9402657fbeba5affaf56d255d9d5bc4dd984458c
ssavoca <ssavoca@g.clemson.edu>
2017-02-23 16:26:37 -0500

    changes made from file given

--- app.js
+++ app.js
@@ -7,10 +7,9 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
-	request = require('request'),
-	
-	= require('lodash');
+  request = require('request'),
 
+_ = require('lodash');
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
@@ -25,22 +24,24 @@ app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 
 
-app.get('process_get', function (req, res) {
-	//Prepare output in JSON format
-	response = {
-		latitude:req.query.latitude,
-		longitude:req.query.longitude
-	};
-	
-	var callURL = "https://27b63554-1d16-4e55-b39b-fcf26579c187:cTvShACMby@twservice.mybluemix.net/api/weather/v1/geocode/"+response.latitude+"/"+response.longitude+"/forecast/hourly/48hour.json?units=m&language=en-US
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      latitude:req.query.latitude,
+      longitude:req.query.longitude
+     };
 
-	request.get(callURL, {
-	json: true
-	},
-	function (error, response, body) {
-		console.log(body)
-	});
+      var callURL = "https://27b63554-1d16-4e55-b39b-fcf26579c187:cTvShACMby@twcservice.mybluemix.net/api/weather/v1/geocode/"+response.latitude+"/"+response.longitude+"/forecast/hourly/48hour.json?units=m&language=en-US"
+
+      request.get(callURL, {
+        json: true
+      },
+      function (error, response, body) {
+       console.log(body)
+      });
 })
+
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
